82203b8fd5a437fc97d080352aa6af5944cb9282
unknown <nitin786786>
2019-10-01 18:50:05 +0530

    This is initial portfolio commit

---
+++ src/app/portfoli-dashboard/portfoli-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfoli-dashboard',
+  templateUrl: './portfoli-dashboard.component.html',
+  styleUrls: ['./portfoli-dashboard.component.css']
+})
+export class PortfoliDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
