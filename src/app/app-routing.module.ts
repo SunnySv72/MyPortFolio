0337aaf1a2a82d0329ff8256f025050ae259bc87
unknown <nitin786786>
2019-10-03 17:59:05 +0530

    Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfoliDashboardComponent } from '../app/portfoli-dashboard/portfoli-dashboard.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: PortfoliDashboardComponent},
+  {path: 'details', component: UserDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
