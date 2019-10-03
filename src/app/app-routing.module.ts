2c8351987f81882cc3113683fa03349cce3fb264
unknown <nitin786786>
2019-10-03 18:20:07 +0530

    Added Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortfoliDashboardComponent } from '../app/portfoli-dashboard/portfoli-dashboard.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: PortfoliDashboardComponent},
-  {path: 'details', component: UserDetailsComponent}
+  {path: 'details', component: UserDetailsComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
